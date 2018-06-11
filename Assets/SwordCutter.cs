ab5a1b74feb44380d995afa9aa959ea63d47b14a
Surabhi98 <s.agarwal0278@gmail.com>
2018-06-11 13:17:18 +0100

    First

---
+++ Assets/SwordCutter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+public class SwordCutter : MonoBehaviour {
+
+    public Material capMaterial;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+
+        GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
+
+        if (!pieces[1].GetComponent<Rigidbody>())
+        {
+            pieces[1].AddComponent<Rigidbody>();
+            MeshCollider temp = pieces[1].AddComponent<MeshCollider>();
+            temp.convex = true;
+
+        }
+        Destroy(pieces[1], 1);
+    }
+
+
+
+
+
+
+}
